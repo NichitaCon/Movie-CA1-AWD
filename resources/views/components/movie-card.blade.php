e1f7b5e687f44c293278624e6b01cd2914739b23
Nichita <N00233189@iadt.ie>
2024-11-15 00:20:25 +0000

    view movie components FIXED/WORKING (L5)

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -2,7 +2,7 @@
 
 <div class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300">
     <h4 class="font-bold text-lg">{{ $name }}</h4>
-    <img src="{{asset( 'public\images\movies' . $image_id)}}" alt="{{$name}}">
+    <img src="{{asset( 'images\movies/' . $image_id)}}" alt="{{$name}}">
     <p class="text-gray-600">Pg rating: {{ $pg_rating }}</p>
     <p class="text-gray-800 mt-3">Rating: {{ $rating }}</p>
     <p class="text-gray-800 mt-4">{{ $description }}</p>
