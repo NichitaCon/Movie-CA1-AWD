afed36f5dba96073992c807ee315fdfdc4e18ea7
Nichita <N00233189@iadt.ie>
2024-11-19 23:12:17 +0000

    Create movie working, no bugs (L7)

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'pg_rating',
+        'rating',
+        'budget',
+        'release_date',
+        'running_time',
+        'image_id',
+    ];
 }
